e156270e3dc87a9ad2e46c7a38dab169967d2d05
pauleveritt <paul.everitt@jetbrains.com>
2024-05-26 09:32:51 -0400

    Get all the names etc. to match and first post.

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-  title: "Eleventy Base Blog v8",
-  url: "https://example.com/",
+  title: "Chatterbox",
+  url: "https://www.pauleveritt.org/",
   language: "en",
-  description: "I am writing about my experiences as a naval navel-gazer.",
+  description: "Paul Everitt personal blog",
   author: {
-    name: "Your Name Here",
-    email: "youremailaddress@example.com",
-    url: "https://example.com/about-me/",
+    name: "Paul Everitt",
+    email: "pauleveritt@me.com",
+    url: "https://www.pauleveritt.org/about/",
   },
 };
--- content/about/index.md
+++ content/about/index.md
@@ -4,6 +4,13 @@ eleventyNavigation:
   key: About Me
   order: 3
 ---
+
 # About Me
 
-I am a person that writes stuff.
+I'm a Python and Web Developer Advocate at JetBrains. Before that, I
+was a co-founder of Zope Corporation, taking the first open source application
+server through $14M of funding. Prior to that, I was an officer in the
+US Navy, starting www.navy.mil in 1993.
+
+I've done open source organizing stuff: I bootstrapped both the Python
+Software Foundation and the Plone Foundation.
