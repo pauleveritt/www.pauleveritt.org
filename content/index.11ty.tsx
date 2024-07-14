e3336ab8436ca93f7ce1ca58860c8df43039ed52
pauleveritt <paul.everitt@jetbrains.com>
2024-07-14 10:30:37 -0400

    Change index page to use the PostHeader/Footer component.

--- content/index.11ty.tsx
+++ content/index.11ty.tsx
@@ -1,16 +1,7 @@
 import { BaseLayout } from "../components/BaseLayout";
 import { PostItem, PostList } from "../components/PostList";
-
-function head(array: any[], n: number): any[] {
-  if (!Array.isArray(array) || array.length === 0) {
-    return [];
-  }
-  if (n < 0) {
-    return array.slice(n);
-  }
-
-  return array.slice(0, n);
-}
+import { PostsHeader } from "../components/PostsHeader";
+import { PostsFooter } from "../components/PostsFooter";
 
 export type IndexPageData = {
   description: string;
@@ -32,26 +23,13 @@ export default class IndexPage {
   }
 
   render(data: IndexPageData) {
-    const numberOfLatestPostsToShow = 3;
     const { posts } = data.collections;
-    const postsCount = posts.length;
-    const latestPostsCount = Math.min(postsCount, numberOfLatestPostsToShow);
 
-    const postsList = head(posts, -numberOfLatestPostsToShow);
-    const morePosts = postsCount - numberOfLatestPostsToShow;
     return (
       <BaseLayout>
-        <h1>
-          Latest {latestPostsCount > 1 ? latestPostsCount : ""} Post
-          {latestPostsCount > 1 ? "s" : ""}
-        </h1>
-        <PostList postItems={postsList} />
-        {morePosts > 0 && (
-          <p title="More Posts">
-            {morePosts} more post{morePosts > 1 ? "s" : ""} can be found in{" "}
-            <a href="/blog/">the archive</a>.
-          </p>
-        )}
+        <PostsHeader collection={posts} />
+        <PostList postItems={posts} />
+        <PostsFooter collectionCount={posts.length} />
       </BaseLayout>
     );
   }
